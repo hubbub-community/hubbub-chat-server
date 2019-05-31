8cb5e118dcf66d7a45f1e8bb0b8508b4d1ac59b5
Joseph <z@charmed.tech>
2019-05-31 13:12:01 -0700

    Update arguments in about.test.ts

--- src/events/command-handlers/__tests__/about.test.ts
+++ src/events/command-handlers/__tests__/about.test.ts
@@ -6,15 +6,23 @@ import about from '../about'
 jest.mock('../../lib/send-to-user', () => jest.fn())
 
 describe('`about` function', () => {
-  it('should call `sendToUser` with a message, socket, and server', () => {
+  it('should call `sendToUser` with a message, socket, server, and null', () => {
+    const arg: null = null
     const socket: any = {}
     const io: any = {}
-    about(null, socket, io)
+    about(arg, socket, io)
     expect(sendToUser).toHaveBeenCalledWith(
       expect.any(String),
       socket,
       io,
       null
     )
+  })
+
+  it('should return `undefined`', () => {
+    const socket: any = {}
+    const io: any = {}
+    const result = about(null, socket, io)
+    expect(result).toBeUndefined()
   })
 })
