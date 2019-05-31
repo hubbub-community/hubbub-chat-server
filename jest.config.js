ee0b57d3e8e4d00de6aeb58fbb2c86c5f2419389
Joseph <z@charmed.tech>
2019-05-31 11:58:45 -0700

    Ignore build and supergoose in `jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/src/__tests__/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/__tests__/',
+    '<rootDir>/build/',
+    'supergoose',
+  ],
 }
