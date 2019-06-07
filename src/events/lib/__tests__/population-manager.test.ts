80b7dc5e371e2d9c625ad1538517bfc37d356eba
Joseph <z@charmed.tech>
2019-06-06 20:42:07 -0700

    Attempt to fix test for population-manager
    Unclear why travis and local results differ.
    
    [Ticket: #8]

--- src/events/lib/__tests__/population-manager.test.ts
+++ src/events/lib/__tests__/population-manager.test.ts
@@ -1,4 +1,4 @@
-import faker from 'faker'
+import * as faker from 'faker'
 
 import * as t from '../../../types/global'
 
@@ -71,47 +71,47 @@ describe('`Population` class', () => {
   describe('`depopulateRoom` method', () => {
     let room: t.TRoomName
     let rooms: t.IRooms
-    let user1: t.TUsername
-    let user2: t.TUsername
+    let username1: t.TUsername
+    let username2: t.TUsername
 
     beforeEach(() => {
       rooms = p.rooms
       room = faker.random.word()
-      user1 = faker.random.number().toString()
-      user2 = faker.random.number().toString()
-      p.populateRoom(user1, room)
-      p.populateRoom(user2, room)
+      username1 = faker.random.number().toString()
+      username2 = faker.random.number().toString()
+      p.populateRoom(username1, room)
+      p.populateRoom(username2, room)
     })
 
     it('should remove a user from a room', () => {
-      p.depopulateRoom(user2, room)
-      const expected: t.IRoom = { leader: user1, users: [user1] }
+      p.depopulateRoom(username2, room)
+      const expected: t.IRoom = { leader: username1, users: [username1] }
       expect(rooms[room]).toEqual(expected)
     })
 
     it('should delete the room if it no longer exists', () => {
-      p.depopulateRoom(user1, room)
-      p.depopulateRoom(user2, room)
+      p.depopulateRoom(username1, room)
+      p.depopulateRoom(username2, room)
       expect(rooms[room]).toBeUndefined()
     })
 
     it('should assign the `leader` property to the person who has been there longest if the room still exists', () => {
-      p.depopulateRoom(user1, room)
-      const expected = { leader: user2, users: [user2] }
+      p.depopulateRoom(username1, room)
+      const expected = { leader: username2, users: [username2] }
       expect(rooms[room]).toEqual(expected)
     })
 
     it('should do nothing if the user is not in the room', () => {
-      const user3: t.TUsername = faker.random.number().toString()
+      const socketId3: t.TUsername = faker.random.number().toString()
       const copy: t.IRoom = Object.assign({}, rooms[room])
-      p.depopulateRoom(user3, room)
+      p.depopulateRoom(socketId3, room)
       expect(copy.users).toEqual(rooms[room].users)
       expect(copy.leader).toEqual(rooms[room].leader)
     })
 
     it('should do nothing if the room does not exist', () => {
-      p.depopulateRoom(user1, room)
-      p.depopulateRoom(user2, room)
+      p.depopulateRoom(username1, room)
+      p.depopulateRoom(username2, room)
       expect(rooms.room).toBeUndefined()
     })
   })
@@ -119,29 +119,30 @@ describe('`Population` class', () => {
   describe('`details` method', () => {
     let room1: t.TRoomName
     let room2: t.TRoomName
-    let user1: t.TSocketId
+
+    let socketId1: t.TSocketId
     let username1: t.TUsername
-    let user2: t.TSocketId
+    let socketId2: t.TSocketId
     let username2: t.TUsername
-    let user3: t.TSocketId
+    let socketId3: t.TSocketId
     let username3: t.TUsername
 
     beforeEach(() => {
       room1 = faker.random.word()
       room2 = faker.random.word()
-      user1 = faker.random.number().toString()
+      socketId1 = faker.random.number().toString()
       username1 = faker.random.word()
-      user2 = faker.random.number().toString()
+      socketId2 = faker.random.number().toString()
       username2 = faker.random.word()
-      user3 = faker.random.number().toString()
+      socketId3 = faker.random.number().toString()
       username3 = faker.random.word()
 
-      p.addUser(user1, username1)
-      p.addUser(user2, username2)
-      p.addUser(user3, username3)
-      p.populateRoom(user1, room1)
-      p.populateRoom(user2, room2)
-      p.populateRoom(user3, room2)
+      p.addUser(socketId1, username1)
+      p.addUser(socketId2, username2)
+      p.addUser(socketId3, username3)
+      p.populateRoom(socketId1, room1)
+      p.populateRoom(socketId2, room2)
+      p.populateRoom(socketId3, room2)
     })
 
     describe('`roomNames` property', () => {
@@ -194,16 +195,16 @@ describe('`Population` class', () => {
 
       it('should have the correct number of users in each key-value array', () => {
         const d: Population = new Population()
-        d.addUser(user1, username1)
-        d.addUser(user2, username2)
-        d.populateRoom(user1, room1)
-        d.populateRoom(user2, room1)
+        d.addUser(socketId1, username1)
+        d.addUser(socketId2, username2)
+        d.populateRoom(socketId1, room1)
+        d.populateRoom(socketId2, room1)
         const { userCountPerRoom } = d.getDetails()
         let count: t.TUserCount = 2
         expect(userCountPerRoom[room1]).toBe(count)
 
-        d.addUser(user3, username3)
-        d.populateRoom(user3, room1)
+        d.addUser(socketId3, username3)
+        d.populateRoom(socketId3, room1)
 
         const ucpr: t.IUserCountPerRoom = d.getDetails().userCountPerRoom
 
@@ -215,16 +216,16 @@ describe('`Population` class', () => {
     describe('`usernamesPerRoom` property', () => {
       it('should have the correct users in each key-value array', () => {
         const e = new Population()
-        e.addUser(user1, username1)
-        e.addUser(user2, username2)
-        e.populateRoom(user1, room1)
-        e.populateRoom(user2, room1)
+        e.addUser(socketId1, username1)
+        e.addUser(socketId2, username2)
+        e.populateRoom(socketId1, room1)
+        e.populateRoom(socketId2, room1)
         const { usernamesPerRoom } = e.getDetails()
         const array = [username1, username2]
         expect(usernamesPerRoom[room1]).toEqual(expect.arrayContaining(array))
 
-        e.addUser(user3, username3)
-        e.populateRoom(user3, room1)
+        e.addUser(socketId3, username3)
+        e.populateRoom(socketId3, room1)
         const newArray: string[] = [username1, username2, username3]
         const unpr = e.getDetails().usernamesPerRoom
         expect(unpr[room1]).toEqual(expect.arrayContaining(newArray))
@@ -234,63 +235,63 @@ describe('`Population` class', () => {
 
   describe('`moveUser method`', () => {
     const f: Population = new Population()
-    let user1: t.TSocketId
+    let socketId1: t.TSocketId
     let username1: t.TUsername
-    let user2: t.TSocketId
+    let socketId2: t.TSocketId
     let username2: t.TUsername
-    let user3: t.TSocketId
+    let socketId3: t.TSocketId
     let username3: t.TUsername
 
     const newRoom: t.TRoomName = faker.random.word()
     const oldRoom: t.TRoomName = faker.random.word()
 
     beforeEach(() => {
-      user1 = faker.random.number().toString()
+      socketId1 = faker.random.number().toString()
       username1 = faker.random.word()
-      user2 = faker.random.number().toString()
+      socketId2 = faker.random.number().toString()
       username2 = faker.random.word()
-      user3 = faker.random.number().toString()
+      socketId3 = faker.random.number().toString()
       username3 = faker.random.word()
 
-      f.addUser(user1, username1)
-      f.addUser(user2, username2)
-      f.addUser(user3, username3)
-      f.populateRoom(user1, oldRoom)
-      f.populateRoom(user2, oldRoom)
-      f.populateRoom(user3, oldRoom)
+      f.addUser(socketId1, username1)
+      f.addUser(socketId2, username2)
+      f.addUser(socketId3, username3)
+      f.populateRoom(socketId1, oldRoom)
+      f.populateRoom(socketId2, oldRoom)
+      f.populateRoom(socketId3, oldRoom)
     })
 
     it('should not expect the target room to exist', () => {
       const initial: t.IRoom = f.rooms.newRoom
       expect(initial).toBeUndefined()
 
-      f.moveUser(user1, oldRoom, newRoom)
+      f.moveUser(socketId1, oldRoom, newRoom)
       const final: t.IRoom = f.rooms[newRoom]
 
       expect(final).toBeDefined()
     })
 
     it('should add a user to a new room', () => {
-      f.moveUser(user1, oldRoom, newRoom)
-      const result: boolean = f.rooms[newRoom].users.includes(user1)
+      f.moveUser(socketId1, oldRoom, newRoom)
+      const result: boolean = f.rooms[newRoom].users.includes(socketId1)
       expect(result).toBeTruthy()
     })
 
     it('should remove the user from the old room', () => {
-      f.moveUser(user1, oldRoom, newRoom)
-      const result: boolean = f.rooms[oldRoom].users.includes(user1)
+      f.moveUser(socketId1, oldRoom, newRoom)
+      const result: boolean = f.rooms[oldRoom].users.includes(socketId1)
       expect(result).toBeFalsy()
     })
 
     it('should not affect other users in the old room', () => {
-      const result: boolean = f.rooms[oldRoom].users.includes(user2)
+      const result: boolean = f.rooms[oldRoom].users.includes(socketId2)
       expect(result).toBeTruthy()
     })
 
     it('should not affect users in the new room', () => {
-      f.moveUser(user1, oldRoom, newRoom)
-      f.moveUser(user2, oldRoom, newRoom)
-      const result: boolean = f.rooms[newRoom].users.includes(user1)
+      f.moveUser(socketId1, oldRoom, newRoom)
+      f.moveUser(socketId2, oldRoom, newRoom)
+      const result: boolean = f.rooms[newRoom].users.includes(socketId1)
       expect(result).toBeTruthy()
     })
   })
